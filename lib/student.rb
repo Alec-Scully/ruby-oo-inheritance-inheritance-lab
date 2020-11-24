220e17e4cf7d1d2131cc36a6f2f25eb5c8667732
Alec Scully <ScullyAlec@gmail.com>
2020-11-24 17:21:38 -0500

    commit tree eb967319c1260e75ec47553f9c80652db5cdff7f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(teacher_knowledge)
+        @knowledge << teacher_knowledge
+    end
+
+    def knowledge 
+        @knowledge
+    end
 
 end
